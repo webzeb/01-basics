439fcfc55ba3032e479b1c223d37fa81095ac136
Keegan Leary <keegan@webbae.net>
2023-11-01 17:16:16 -0700

    remove previous code

--- src/01-2-basics.js
+++ src/01-2-basics.js
@@ -11,22 +11,6 @@
 function generateBandName(clothingColor, lastFoodEaten) {
   // Your code here
   // Initialize bandName to an empty string
-  let bandName = "";
-
   // Function to capitalize the first letter of each word
-  function capitalizeFirstLetter(word) {
-    if (typeof word !== "string" || !word) {
-      return "";
-    }
-    return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-  }
-
   // Construct the band name
-  bandName =
-    "The " +
-    capitalizeFirstLetter(clothingColor) +
-    " " +
-    capitalizeFirstLetter(lastFoodEaten);
-
-  return bandName;
 }
